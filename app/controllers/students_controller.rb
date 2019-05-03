149436f47431de109e686918234a964aa706c843
frank <fniscak@gmail.com>
2019-05-02 22:36:56 -0700

    removed debug messages in students controller

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -16,11 +16,11 @@ class StudentsController < ApplicationController
     @total_num_of_students = @filterrific.find.count
     @current_page = params[:page]
 
-    puts "*" * 70
-    puts "Page Number : " + @current_page.to_s
-    puts "Number of Students in this Page : " + @students.length.to_s
-    puts "TOTAL Number of Students in this SEARCH  : " + @total_num_of_students.to_s
-    puts "*" * 70
+    # puts "*" * 70
+    # puts "Page Number : " + @current_page.to_s
+    # puts "Number of Students in this Page : " + @students.length.to_s
+    # puts "TOTAL Number of Students in this SEARCH  : " + @total_num_of_students.to_s
+    # puts "*" * 70
 
     respond_to do |format|
       format.html
